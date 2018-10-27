3d38c6da706174eca4e0f0a0771852d94c1ece74
Jonathan <jfenn-tye@live.co.uk>
2018-10-27 21:44:19 +0100

    Implemented Basic Models

---
+++ DesireListApplicationv1/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesireListApplicationv1.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Checked { get; set; }
+        public int ListId { get; set; }
+
+        public Item()
+        {
+            Id = 0;
+            Name = string.Empty;
+            Checked = false;
+            ListId = 1;
+        }
+    }
+}
---
+++ DesireListApplicationv1/Models/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesireListApplicationv1.Models
+{
+    public class List
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Item> Items { get; set; }
+
+        public List()
+        {
+            Id = 0;
+            Name = string.Empty;
+            Items = new List<Item>();
+        }
+    }
+}
